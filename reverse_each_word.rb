847511d0be734712e1597a69ae104eb456e1ec4e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-09-05 18:49:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,8 @@ def reverse_each_word(sentence)
     new_sentence << word.reverse
   end
   new_sentence.join(" ")
+end
 
+def reverse_each_word(sentence)
   sentence.split.collect { |word| word.reverse}
 end
